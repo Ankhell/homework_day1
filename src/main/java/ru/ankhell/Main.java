a1d7a4bb99ed2f21aef560c95e77def5307da9e9
Dmitry D. (ankhell) <ankhell.java@gmail.com>
2019-12-19 00:43:54 +0300

    реализован метод проверки входных данных на соответствие HEX,OCT,BIN

--- src/main/java/ru/ankhell/Main.java
+++ src/main/java/ru/ankhell/Main.java
@@ -10,4 +10,24 @@ public class Main {
     public static String BinToHex(String input_hexNumber){
         return  Long.toHexString(Long.parseLong(input_hexNumber,2));
     }
+
+    public static boolean isNumber(String input_string){
+        try {
+            Long.parseLong(input_string, 16);
+        }
+        catch (NumberFormatException num){
+            try {
+                Long.parseLong(input_string, 8);
+            }
+            catch (NumberFormatException num1){
+                try {
+                    Long.parseLong(input_string, 2);
+                }
+                catch (NumberFormatException num3){
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
 }
