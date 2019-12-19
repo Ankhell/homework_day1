b937fb6ce8079e4d47ed23a07ae0e13db24f84e9
Dmitry D. (ankhell) <ankhell.java@gmail.com>
2019-12-19 12:54:10 +0300

    Исправлен testng.xml Метод тестового класса soutThread() перенесён в BeforeMethod Имена обьектов датапровайдеров приведены в соответствие с provider name

--- src/test/java/ru/ankhell/TestMain.java
+++ src/test/java/ru/ankhell/TestMain.java
@@ -1,17 +1,19 @@
 package ru.ankhell;
 
 import org.testng.Assert;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 import static ru.ankhell.Main.*;
 
 public class TestMain {
+    @BeforeMethod
     private void soutThread(){
         System.out.println((Thread.currentThread().getId()));
     }
 
     @DataProvider(name = "intBinProvider", parallel = true)
-    public Object[][] dP1(){
+    public Object[][] intBinProvider(){
         return new Object[][]{
                 {256,"100000000"},{212,"11010100"},{911,"1110001111"}
         };
@@ -19,12 +21,11 @@ public class TestMain {
 
     @Test(description = "Проверка метода intToBin", dataProvider = "intBinProvider")
     public void testIntToBin(int input_integer, String expectedResult){
-        soutThread();
         Assert.assertEquals(intToBin(input_integer),expectedResult);
     }
 
     @DataProvider(name = "intHexProvider", parallel = true)
-    public Object[][] dP2(){
+    public Object[][] intHexProvider(){
         return new Object[][]{
                 {256,"100"},{212,"d4"},{911,"38f"}
         };
@@ -37,7 +38,7 @@ public class TestMain {
     }
 
     @DataProvider(name = "BinHexProvider", parallel = true)
-    public Object[][] dP3(){
+    public Object[][] BinHexProvider(){
         return new Object[][]{
                 {"10001010101101","22ad"},{"100010101","115"},{"1111010101001010011","7aa53"}
         };
@@ -45,40 +46,36 @@ public class TestMain {
 
     @Test(description = "Проверка метода binToHex", dataProvider = "BinHexProvider")
     public void testBinToHex(String input_string, String expectedResult){
-        soutThread();
         Assert.assertEquals(binToHex(input_string),expectedResult);
     }
 
     @DataProvider(name = "notBinStringProvider", parallel = true)
-    public Object[] dp4(){
+    public Object[] notBinStringProvider(){
         return new Object[]{"nan","16ad","2135","400"};
     }
 
     @Test(description = "Проверка реакции метода binToHex на некорректный ввод", dataProvider = "notBinStringProvider",expectedExceptions = NumberFormatException.class)
     public void incorrectInputBinToHex(String inputString){
-        soutThread();
         binToHex(inputString);
     }
 
     @DataProvider(name = "isNumPositive", parallel = true)
-    public Object[] dp5(){
+    public Object[] isNumPositive(){
         return new Object[]{"400","16ad","1010111"};
     }
 
     @Test(description = "Положительный тест метода isNum", dataProvider = "isNumPositive")
     public void isNumPositiveTest(String inputString){
-        soutThread();
         Assert.assertTrue(isNumber(inputString));
     }
 
     @DataProvider(name = "isNumNegative", parallel = true)
-    public Object[] dp6(){
+    public Object[] isNumNegative(){
         return new Object[]{null,"nan","Hello world!"};
     }
 
     @Test(description = "Отрицательный тест метода isNum", dataProvider = "isNumNegative")
     public void isNumNegativeTest(String inputString){
-        soutThread();
         Assert.assertFalse(isNumber(inputString));
     }
 
